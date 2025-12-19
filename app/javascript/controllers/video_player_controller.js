b6638beafee6ed77d43befce3d828100b2451a5e
aleexalcaraz <aleexalcaraz@gmail.com>
2025-12-19 12:00:16 -0600

    add thmbnail

--- app/javascript/controllers/video_player_controller.js
+++ app/javascript/controllers/video_player_controller.js
@@ -2,29 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="video-player"
 export default class extends Controller {
-  static targets = [ "video", "loader" ]
+  static targets = [ "video", "loader", "thumbnail" ]
 
   connect() {
-    // Hide video and show loader
-    this.videoTarget.style.display = 'none';
-    this.loaderTarget.style.display = 'block';
-
     this.showVideo = this.showVideo.bind(this);
     this.showError = this.showError.bind(this);
 
-    // If video is already cached and can play, show it immediately
-    if (this.videoTarget.readyState >= 3) { // HAVE_FUTURE_DATA
-      this.showVideo();
-    } else {
-      // Otherwise, wait for the 'canplay' event
-      this.videoTarget.addEventListener('canplay', this.showVideo);
-      this.videoTarget.addEventListener('error', this.showError);
-    }
+    this.videoTarget.addEventListener('canplay', this.showVideo);
+    this.videoTarget.addEventListener('error', this.showError);
   }
 
   disconnect() {
     this.videoTarget.removeEventListener('canplay', this.showVideo);
     this.videoTarget.removeEventListener('error', this.showError);
+  }
+
+  play() {
+    this.thumbnailTarget.style.display = 'none';
+    this.loaderTarget.style.display = 'block';
+    this.videoTarget.load();
+    this.videoTarget.play();
   }
 
   showVideo() {
