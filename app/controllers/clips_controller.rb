e4552073d3bcb1b508497cb518ec5d56ae26dd9d
aleexalcaraz <aleexalcaraz@gmail.com>
2025-12-19 12:51:40 -0600

    trying to fix iphone

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -48,22 +48,58 @@ class ClipsController < ApplicationController
         end
     end
 
+    # def serve
+    #     filename = params[:filename]
+        
+    #     disposition = params[:disposition] == 'download' ? 'attachment' : 'inline'
+
+    #     full_path = File.join(CLIPS_ROOT_PATH, filename)   
+
+    #     unless full_path.start_with?(CLIPS_ROOT_PATH) && File.exist?(full_path)
+    #     head :not_found and return
+    #     end
+
+    #     send_file(
+    #         full_path,
+    #         filename: filename,
+    #         type: 'video/mp4',
+    #         disposition: disposition
+    #     )
+    # end
+
     def serve
         filename = params[:filename]
-        
         disposition = params[:disposition] == 'download' ? 'attachment' : 'inline'
-
-        full_path = File.join(CLIPS_ROOT_PATH, filename)   
+        full_path = File.join(CLIPS_ROOT_PATH, filename)
 
         unless full_path.start_with?(CLIPS_ROOT_PATH) && File.exist?(full_path)
-        head :not_found and return
+            head :not_found and return
         end
 
-        send_file(
-            full_path,
-            filename: filename,
-            type: 'video/mp4',
-            disposition: disposition
-        )
+        file_size = File.size(full_path)
+
+        if request.headers['Range']
+            # iOS sends a header like "bytes=0-1"
+            range = request.headers['Range'].match(/bytes=(\d+)-(\d*)/)
+            start_byte = range[1].to_i
+            end_byte = range[2].to_i.zero? ? file_size - 1 : range[2].to_i
+            length = end_byte - start_byte + 1
+
+            # Important headers for iOS streaming
+            response.headers['Content-Range'] = "bytes #{start_byte}-#{end_byte}/#{file_size}"
+            response.headers['Accept-Ranges'] = 'bytes'
+            
+            # We send only the requested chunk of data
+            send_data IO.binread(full_path, length, start_byte), 
+                    type: 'video/mp4', 
+                    disposition: disposition, 
+                    status: :partial_content # This sends Status 206
+        else
+            # Standard request for the whole file
+            response.headers['Accept-Ranges'] = 'bytes'
+            send_file full_path, 
+                    type: 'video/mp4', 
+                    disposition: disposition
+        end
     end
 end
--- app/javascript/controllers/video_player_controller.js
+++ app/javascript/controllers/video_player_controller.js
@@ -1,35 +1,40 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="video-player"
 export default class extends Controller {
-  static targets = [ "video", "loader", "thumbnail" ]
+  static targets = ["video", "loader"];
 
   connect() {
+    // Hide video and show loader
+    this.videoTarget.style.display = "none";
+    this.loaderTarget.style.display = "block";
+
     this.showVideo = this.showVideo.bind(this);
     this.showError = this.showError.bind(this);
 
-    this.videoTarget.addEventListener('canplay', this.showVideo);
-    this.videoTarget.addEventListener('error', this.showError);
+    // If video is already cached and can play, show it immediately
+    if (this.videoTarget.readyState >= 3) {
+      // HAVE_FUTURE_DATA
+      this.showVideo();
+    } else {
+      // Otherwise, wait for the 'canplay' event
+      this.videoTarget.addEventListener("canplay", this.showVideo);
+      this.videoTarget.addEventListener("error", this.showError);
+    }
   }
 
   disconnect() {
-    this.videoTarget.removeEventListener('canplay', this.showVideo);
-    this.videoTarget.removeEventListener('error', this.showError);
-  }
-
-  play() {
-    this.thumbnailTarget.style.display = 'none';
-    this.loaderTarget.style.display = 'block';
-    this.videoTarget.load();
-    this.videoTarget.play();
+    this.videoTarget.removeEventListener("canplay", this.showVideo);
+    this.videoTarget.removeEventListener("error", this.showError);
   }
 
   showVideo() {
-    this.loaderTarget.style.display = 'none';
-    this.videoTarget.style.display = 'block';
+    this.loaderTarget.style.display = "none";
+    this.videoTarget.style.display = "block";
   }
 
   showError() {
-    this.loaderTarget.innerHTML = '<p class="text-danger">Error loading video.</p>';
+    this.loaderTarget.innerHTML =
+      '<p class="text-danger">Error loading video.</p>';
   }
 }
